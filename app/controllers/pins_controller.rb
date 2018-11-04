73fded5875a841af029027644b264be9420fb599
Ali Zahir <alizahir@live.com>
2018-11-03 20:27:33 -0400

    pushed the latest version

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,24 @@
+class PinsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: "Saved Successfully"
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
+end
